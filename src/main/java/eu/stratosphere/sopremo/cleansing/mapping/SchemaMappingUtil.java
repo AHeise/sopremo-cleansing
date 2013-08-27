ac3a4ff67c65c6e7662c3dc09437d38dc604a3c7
claudialehmann <claudia_lehmann89@gmx.de>
2013-08-27 18:59:06 +0200

    Schema Mapping Operator

---
+++ src/main/java/eu/stratosphere/sopremo/cleansing/mapping/SchemaMappingUtil.java
@@ -0,0 +1,59 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2012 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.sopremo.cleansing.mapping;
+
+import static eu.stratosphere.sopremo.type.JsonUtil.createPath;
+import it.unibas.spicy.model.paths.SetAlias;
+import it.unibas.spicy.model.paths.VariablePathExpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.stratosphere.sopremo.expressions.PathSegmentExpression;
+
+
+/**
+ * @author Andrina Mascher, Arvid Heise
+ *
+ */
+public class SchemaMappingUtil {
+
+	public static PathSegmentExpression convertSpicyPath(String inputIndex, VariablePathExpression spicyPath) {
+		/* e.g. spicyPath 					= 	v0.usCongressMember.biography
+		 * spicyPath.getAbsolutePath()	 	= 	usCongress.usCongressMembers.usCongressMember.biography
+		 * spicyPath.getPathSteps()			=	[usCongressMember, biography]
+		 * spicyPath.getStartingVariable()	=	v0 in usCongress.usCongressMembers
+		 */
+
+		// create steps such as [0, v0, biography]
+		List<String> pathSteps = new ArrayList<String>(); 
+		if( inputIndex!=null ) {
+			pathSteps.add( inputIndex ); //needed for multiple input operator e.g. join input 0 or 1
+		} else {
+			pathSteps.add( "0" ); //TODO test
+		}
+		pathSteps.add( getSourceId(spicyPath.getStartingVariable()) ); //e.g. v0
+	
+		for(int i=1; i<spicyPath.getPathSteps().size(); i++) { //always ignore [0], is replaced by sourceId v0
+			pathSteps.add( spicyPath.getPathSteps().get(i) );			
+		}
+		
+		return createPath(pathSteps); 
+	}
+	
+	public static String getSourceId(SetAlias setAlias) { 
+		return "v"+ setAlias.getId(); //e.g. "v0"
+	}
+}
---
+++ src/test/java/eu/stratosphere/sopremo/cleansing/mapping/SchemaMappingIT.java
@@ -0,0 +1,79 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.sopremo.cleansing.mapping;
+
+import static eu.stratosphere.sopremo.type.JsonUtil.createObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.stratosphere.meteor.MeteorIT;
+import eu.stratosphere.sopremo.operator.SopremoPlan;
+
+public class SchemaMappingIT extends MeteorIT {
+	private File usCongressMembers, usCongressBiographies, person, legalEntity;
+
+	@Before
+	public void createInput() throws IOException {
+		this.usCongressMembers = this.testServer.createFile("usCongressMembers.json", 
+				createObjectNode("id", "usCongress1", "name", "Andrew Adams", "biography", "A000029"),
+				createObjectNode("id", "usCongress2", "name", "John Adams", "biography", "A000039"), 
+				createObjectNode("id", "usCongress3", "name", "John Doe", "biography", "A000059"));
+		this.usCongressBiographies = this.testServer.createFile("usCongressBiographies.json", 
+				createObjectNode("biographyId", "A000029", "worksFor", "CompanyXYZ"),
+				createObjectNode("biographyId", "A000059", "worksFor", "CompanyUVW"),
+				createObjectNode("biographyId", "A000049", "worksFor", "CompanyABC"));
+		this.person = this.testServer.getOutputFile("person.json");
+		this.legalEntity = this.testServer.getOutputFile("legalEntity.json");
+	}
+	
+	@Test
+	public void testNaive() throws IOException {
+
+		String query = "$usCongressMembers = read from '" + this.usCongressMembers.toURI() + "';\n" +
+				"$usCongressBiographies = read from '" + this.usCongressBiographies.toURI() + "';\n" +
+				"$person, $legalEntity = map entities from $usCongressMembers, $usCongressBiographies\n" +
+				"where ($usCongressMembers.biography[0:1] == $usCongressBiographies.biographyId[1:1])\n" + 
+				"as [\n" +  
+				"  group $usCongressMembers by $usCongressMembers.id into {" + 
+				"    name: $usCongressMembers.name,\n" +
+				"    worksFor: $legalEntity.id" + 
+				"  }," + 
+				"  group $usCongressBiographies by $usCongressBiographies.worksFor into {" + 
+				"    name: $usCongressBiographies.worksFor" + 
+				"  }" + 
+				"];\n" + 
+				"write $person to '" + this.person.toURI() + "';\n" +
+				"write $legalEntity to '" + this.legalEntity.toURI() + "';";
+		
+		final SopremoPlan plan = parseScript(query);
+		
+		Assert.assertNotNull(this.client.submit(plan, null, true));
+		
+		this.testServer.checkContentsOf("person.json",
+				createObjectNode("id", "usCongress1", "name", "Andrew Adams", "worksFor", "CompanyXYZ"),
+				createObjectNode("id", null, "name", null, "worksFor", "CompanyABC"),
+				createObjectNode("id", "usCongress3", "name", "John Doe", "worksFor", "CompanyUVW"));
+		
+		this.testServer.checkContentsOf("legalEntity.json",
+				createObjectNode("id", "CompanyXYZ", "name", "CompanyXYZ"),
+				createObjectNode("id", "CompanyUVW", "name", "CompanyUVW"),
+				createObjectNode("id", "CompanyABC", "name", "CompanyABC"));
+	}
+}
