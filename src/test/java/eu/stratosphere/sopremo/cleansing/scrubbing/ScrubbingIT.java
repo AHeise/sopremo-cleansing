3d14f634c5c383923d86a84eb3aa77b8d04438e5
Tommy Neubert <tommy.neubert@student.hpi.uni-potsdam.de>
2013-08-30 11:25:45 +0200

    fixed some minor bugs in ScrubbingIT

--- src/test/java/eu/stratosphere/sopremo/cleansing/scrubbing/ScrubbingIT.java
+++ src/test/java/eu/stratosphere/sopremo/cleansing/scrubbing/ScrubbingIT.java
@@ -9,16 +9,25 @@ import org.junit.Test;
 import eu.stratosphere.meteor.MeteorIT;
 import eu.stratosphere.sopremo.io.JsonParser;
 import eu.stratosphere.sopremo.operator.SopremoPlan;
+import eu.stratosphere.sopremo.type.IArrayNode;
+import eu.stratosphere.sopremo.type.IJsonNode;
 
 public class ScrubbingIT extends MeteorIT {
 
 	@Test
 	public void testSuccessfulExecution() throws IOException {
-		final SopremoPlan plan = parseScript(new File("src/test/resources/ScrubbingIT.script"));
+		final SopremoPlan plan = parseScript(new File(
+				"src/test/resources/ScrubbingIT.script"));
 
 		this.client.submit(plan, null, true);
-		final JsonParser parser = new JsonParser(new FileReader("src/test/resources/TestOutput.json"));
-		parser.setWrappingArraySkipping(true);
-		this.testServer.checkContentsOf("TestOutput.json", parser.readValueAsTree());
+		final JsonParser parser = new JsonParser(new FileReader(
+				"src/test/resources/TestOutput.json"));
+		parser.setWrappingArraySkipping(false);
+		IJsonNode expectedValues = parser.readValueAsTree();
+		IJsonNode[] nodeArray = new IJsonNode[((IArrayNode)expectedValues).size()];
+		for(int i = 0; i < nodeArray.length; i++) {
+			nodeArray[i] = ((IArrayNode)expectedValues).get(i);
+		}
+		this.testServer.checkContentsOf("TestOutput.json", nodeArray);
 	}
 }
